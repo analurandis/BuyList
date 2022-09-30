ffcf7b09487d88322dfa5a8a9179f970fa356139
Ana Lucia dos Santos <ana.lucia.dos.santos@avanade.com>
2022-09-30 15:37:01 -0300

    Code First Azure conection

---
+++ Buylist.Common.Repository.Entity/BuyListRepository.cs
@@ -0,0 +1,47 @@
+﻿using Buylist.CommonRepository;
+using Microsoft.EntityFrameworkCore;
+
+namespace Buylist.Common.Repository.Entity
+{
+    public abstract class BuyListRepository<TEntity, TKey> : IBuyListRepository<TEntity, TKey>
+        where TEntity : class
+    {
+
+        private DbContext _context;
+
+        public BuyListRepository(DbContext dbContext)
+        {
+            _context = dbContext;
+        }
+
+        public List<TEntity> All()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity ByKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteByKey(TKey key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Buylist.CommonRepository/IBuyListRepository.cs
@@ -0,0 +1,18 @@
+﻿namespace Buylist.CommonRepository
+{
+    public interface IBuyListRepository<TEntity, TKey>
+    where TEntity : class
+    {
+
+        List<TEntity> All();
+
+        TEntity ByKey(TKey key);
+
+        void Insert(TEntity entity);
+        void Update(TEntity entity);
+        void Delete(TEntity entity);
+        void DeleteByKey(TKey key);
+
+
+    }
+}
